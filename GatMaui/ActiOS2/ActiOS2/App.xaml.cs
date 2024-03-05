9ce43d1bf7d90a6be232c1722c98e877f8d83414
JuanAlbertoRobles <113071003+JuanAlbertoRobles@users.noreply.github.com>
2024-03-05 15:37:14 -0600

    Gato MAUI sin frontend

---
+++ GatMaui/ActiOS2/ActiOS2/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ActiOS2
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
